be8319112b2d06f055ca38b0c879ec45e5544cd2
tonyn258 <tonyn258@gmail.com>
2026-06-13 16:55:23 -0300

    Atualiza código

--- teste_pedido.py
+++ teste_pedido.py
@@ -1,21 +1,35 @@
 # ===========================
 # ARQUIVO: teste_pedido.py
-# # ===========================
+# ===========================
 
-# from model.pedido_model import PedidoModel
+from model.pedido_model import PedidoModel
 
-# ###################################################
-# # LISTAR PEDIDOS
-# ###################################################
 
-# n = PedidoModel()
+# =====================================================
+# TESTE 1 - LISTAR TODOS OS PEDIDOS
+# =====================================================
+# pedido = PedidoModel()
+# print(pedido.listar())
 
-# print(n.listar())
 
-###################################################
-# ATUALIZAR PEDIDO
-###################################################
+# =====================================================
+# TESTE 2 - SALVAR PEDIDO
+# =====================================================
+# pedido = PedidoModel()
 
+# pedido.cliente_id = 1
+# pedido.descricao = "Pizza Calabresa Grande"
+# pedido.valor = 69.90
+# pedido.data = "2026-06-11"
+
+# pedido.salvar()
+
+# print("Pedido salvo com sucesso!")
+
+
+# =====================================================
+# TESTE 3 - ATUALIZAR PEDIDO
+# =====================================================
 # pedido = PedidoModel()
 
 # pedido.id = 1
@@ -29,44 +43,21 @@
 # print("Pedido atualizado com sucesso!")
 
 
-###########################################################
-#excluir
-###########################################################
-#  from model.pedido_model import PedidoModel
-
+# =====================================================
+# TESTE 4 - EXCLUIR PEDIDO
+# =====================================================
 # pedido = PedidoModel()
 
 # pedido.id = 1
 
 # pedido.excluir()
 
-# print("Pedido excluído!")
+# print("Pedido excluído com sucesso!")
 
 
-#######################################
-#Salvar
-########################################
-
-# from model.pedido_model import PedidoModel
-
-# pedido = PedidoModel()
-
-# pedido.cliente_id = 1
-# pedido.descricao = "Pizza Calabresa Grande"
-# pedido.valor = 69.90
-# pedido.data = "2026-06-11"
-
-# pedido.salvar()
-
-# print("Pedido salvo com sucesso!")
-
-#######################################
-#listar
-########################################
-
-
-from model.pedido_model import PedidoModel
-
+# =====================================================
+# TESTE 5 - BUSCAR PEDIDO POR ID
+# =====================================================
 pedido = PedidoModel()
 
 resultado = pedido.buscar_por_id(2)
